adc6f1d9a1830cecb5393d791c8775ef6568beff
euan.gwd <euan@amwic.co.za>
2018-06-12 14:34:06 +0200

    init branch

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,25 @@
 import React from "react";
+import Link from "gatsby-link";
+import styled from "react-emotion";
 
 const NotFoundPage = () => (
   <div>
     <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <p>You just found a page that doesn&#39;t exist... the sadness.</p>
+    <NavLink to="/">Home</NavLink>
   </div>
 );
 
 export default NotFoundPage;
+
+const NavLink = styled(Link)`
+  /* text-align: center; */
+  color: #064614;
+  font: bold 18px "'Georgia Bold', Times, serif";
+  text-transform: uppercase;
+  padding: 0.5rem;
+
+  :hover {
+    color: #678c68;
+  }
+`;
